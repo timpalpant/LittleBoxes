5f0828e93a4f6337b5e80f9066dfb29c2e0fed48
Timothy Palpant <tim@palpant.us>
2016-02-28 08:46:31 -0500

    Refactor the Crossword class to be a collection of XWClues.

--- lib-python/littleboxes/xword.py
+++ lib-python/littleboxes/xword.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+import enum
 import logging
 import puz
 
@@ -6,121 +8,122 @@ class InvalidCrosswordException(Exception):
     pass
 
 
-class Crossword(object):
-    BLACK_SQUARE = '.'
+@enum.unique
+class XWDirection(str, enum.Enum):
+    ACROSS = 'A'
+    DOWN = 'D'
 
-    def __init__(self, width, height, across, down, black_squares):
+
+XWCoordinate = namedtuple('XWCoordinate', ['num', 'direction'])
+# Represents an individual clue in a Crossword puzzle:
+#    coord (XWCoordinate): The number and direction of this clue.
+#    text (str): The clue text.
+#    box_indices (list(int)): Indices into the location of the boxes
+#        that this clue references. Indices are linear, row-major order.
+#        For across clues, they should be sequential; for down clues
+#        they will be offset by the width of the Crossword.
+XWClue = namedtuple('XWClue', ['coord', 'text', 'box_indices'])
+
+
+class Crossword(object):
+    '''A Crossword puzzle is a set of XWClues arranged on an WxH grid,
+    and an associated fill of letters in that grid.
+    '''
+    black_square = object()
+    logger = logging.getLogger('littleboxes.xword.Crossword')
+
+    def __init__(self, width, height, clues):
         self.width = width
         self.height = height
-        self.across = {clue['num']: clue for clue in across}
-        self.down = {clue['num']: clue for clue in down}
-        self.solution = [None for i in xrange(width * height)]
-        for bs in black_squares:
-            self.solution[bs] = self.BLACK_SQUARE
+        self.clues = clues
+        self.solution = [None for _ in xrange(width * height)]
+        self._fill_black_squares()
         self._validate()
+
+    def _fill_black_squares(self):
+        '''Goes through the set of clues, and marks squares which are not
+        involved in any clue (black squares in the puzzle). These squares
+        should be considered "filled" and never touched.
+        '''
+        touched = [False for _ in self.solution]
+        for clue in self.clues:
+            for idx in clue.box_indices:
+                touched[idx] = True
+        for idx, t in enumerate(touched):
+            if not t:
+                self.solution[idx] = self.black_square
+
+    def _validate(self):
+        for clue in self.clues:
+            for idx in clue.box_indices:
+                if idx < 0 or idx >= len(self.solution):
+                    raise InvalidCrosswordException("Invalid clue indices")
+                if self.solution[idx] == self.black_square:
+                    raise InvalidCrosswordException("Clue with black square?!")
 
     @classmethod
     def load(cls, istream):
         p = puz.load(istream.read())
         cn = p.clue_numbering()
-        black_squares = [i for i, c in enumerate(p.fill)
-                         if c == cls.BLACK_SQUARE]
-        return cls(p.width, p.height, cn.across, cn.down, black_squares)
+        clues = []
+        for clue in cn.across:
+            coord = XWCoordinate(num=clue['num'], direction=XWDirection.ACROSS)
+            indices = [clue['cell'] + i for i in xrange(clue['len'])]
+            xwc = XWClue(coord, clue['clue'], indices)
+            clues.append(xwc)
+        for clue in cn.down:
+            coord = XWCoordinate(num=clue['num'], direction=XWDirection.DOWN)
+            indices = [clue['cell'] + i * p.width for i in xrange(clue['len'])]
+            xwc = XWClue(coord, clue['clue'], indices)
+            clues.append(xwc)
+        return cls(p.width, p.height, clues)
 
-    def _validate(self):
-        for clue in self.across.itervalues():
-            squares = self.get_across(clue['num'])
-            if any(s == self.BLACK_SQUARE for s in squares):
-                raise InvalidCrosswordException(
-                    "%d-across contains a black square?! (%s: %s): %s" % (
-                        clue['num'], clue, squares, self.solution))
-        for clue in self.down.itervalues():
-            squares = self.get_down(clue['num'])
-            if any(s == self.BLACK_SQUARE for s in squares):
-                raise InvalidCrosswordException(
-                    "%d-down contains a black square?! (%s: %s): %s" % (
-                        clue['num'], clue, squares, self.solution))
+    def get_fill(self, clue):
+        '''Returns the current fill for the given clue.
+        Unfilled squares are indicated by None.
 
-    def row(self, i):
-        return i / self.width
+        Args:
+            clue (XWClue): The clue to get the current fill for.
 
-    def col(self, i):
-        return i % self.width
+        Returns:
+            list(char or None): The current fill for this clue.
+        '''
+        return [self.solution[idx] for idx in clue.box_indices]
 
-    def index(self, row, col):
-        return row * self.width + col
+    def set_fill(self, clue, answer):
+        '''Fill in the letters for the given clue with the provided answer.
 
-    def is_set(self, row, col):
-        i = self.index(row, col)
-        return (self.solution[i] is not None)
+        Args:
+            clue (XWClue): The clue we are filling in.
+            answer (str or list(char)): The letters to fill in for this clue.
 
-    def n_set(self):
-        return sum(l is not None for l in self.solution)
-
-    def would_conflict_across(self, num, answer):
-        if num not in self.across:
-            raise ValueError("No such clue %d-across" % num)
-        if len(answer) != self.across[num]['len']:
+        Raises:
+            ValueError if the provided answer is the wrong length for this clue
+            InvalidCrosswordException if the provided answer conflicts with an
+                existing letter that has already been set.
+        '''
+        if len(answer) != len(clue.box_indices):
             raise ValueError("Answer '%s' is not the right length "
-                             "for %d-across (%d != %d)" % (
-                                 answer, num, len(answer), self.across[num]['len']))
-        i = self.across[num]['cell']
-        for j, letter in enumerate(answer):
-            cur = self.solution[i + j]
-            if cur is not None and cur != letter:
+                             "for %s (%d != %d)" % (
+                                 answer, clue,
+                                 len(answer), len(clue.box_indices)))
+        if self.would_conflict(clue, answer):
+            raise InvalidCrosswordException(
+                "Cannot set %s to %s (currently %s)" % (
+                    clue, answer, self.get_fill(clue)))
+        for idx, letter in zip(clue.box_indices, answer):
+            self.solution[idx] = letter
+
+    def would_conflict(self, clue, answer):
+        '''Return whether or not the proposed answer for this clue conflicts
+        with any letters that have already been filled.
+        '''
+        if len(answer) != len(clue.box_indices):
+            raise ValueError("Answer '%s' is not the right length "
+                             "for %s (%d != %d)" % (
+                                 answer, clue,
+                                 len(answer), len(clue.box_indices)))
+        for idx, letter in zip(clue.box_indices, answer):
+            if self.solution[idx] is not None and self.solution[idx] != letter:
                 return True
         return False
-
-    def get_across(self, num):
-        if num not in self.across:
-            raise ValueError("No such clue %d-across" % num)
-        i = self.across[num]['cell']
-        l = self.across[num]['len']
-        return [self.solution[i + j] for j in xrange(l)]
-
-    def set_across(self, num, answer):
-        if num not in self.across:
-            raise ValueError("No such clue %d-across" % num)
-        if len(answer) != self.across[num]['len']:
-            raise ValueError("Answer '%s' is not the right length "
-                             "for %d-across (%d != %d)" % (
-                                 answer, num, len(answer), self.across[num]['len']))
-        logging.debug("Setting %d-across (%s) = %s",
-                      num, self.across[num]['clue'], answer)
-        i = self.across[num]['cell']
-        for j, letter in enumerate(answer):
-            self.solution[i + j] = letter
-
-    def would_conflict_down(self, num, answer):
-        if num not in self.down:
-            raise ValueError("No such clue %d-down" % num)
-        if len(answer) != self.down[num]['len']:
-            raise ValueError("Answer '%s' is not the right length "
-                             "for %d-down (%d != %d)" % (
-                                 answer, num, len(answer), self.down[num]['len']))
-        i = self.down[num]['cell']
-        for j, letter in enumerate(answer):
-            cur = self.solution[i + j * self.width]
-            if cur is not None and cur != letter:
-                return True
-        return False
-
-    def get_down(self, num):
-        if num not in self.down:
-            raise ValueError("No such clue %d-down" % num)
-        i = self.down[num]['cell']
-        l = self.down[num]['len']
-        return [self.solution[i + j * self.width] for j in xrange(l)]
-
-    def set_down(self, num, answer):
-        if num not in self.down:
-            raise ValueError("No such clue %d-down" % num)
-        if len(answer) != self.down[num]['len']:
-            raise ValueError("Answer '%s' is not the right length "
-                             "for %d-down (%d != %d)" % (
-                                 answer, num, len(answer), self.down[num]['len']))
-        logging.debug("Setting %d-down (%s) = %s",
-                      num, self.down[num]['clue'], answer)
-        i = self.down[num]['cell']
-        for j, letter in enumerate(answer):
-            self.solution[i + j * self.width] = letter
---
+++ lib-python/littleboxes/xword_test.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+
+from littleboxes.xword import Crossword, XWDirection
+
+
+class TestCrossword(unittest.TestCase):
+    TEST_PUZZLE = os.path.join(os.path.dirname(__file__),
+                               '..', '..', 'data', 'nyt',
+                               'daily-2015-12-13.puz')
+
+    def setUp(self):
+        self.x = Crossword.load(open(self.TEST_PUZZLE))
+
+    def test_load(self):
+        self.assertEqual(len(self.x.clues), 138)
+
+    def test_get_across(self):
+        clue = self.x.clues[0]
+        assert clue.coord.direction == XWDirection.ACROSS
+        fill = self.x.get_fill(clue)
+        self.assertListEqual(fill, [None, None, None, None, None])
+
+        self.x.solution[clue.box_indices[3]] = 'A'
+        fill = self.x.get_fill(clue)
+        self.assertListEqual(fill, [None, None, None, 'A', None])
+
+    def test_get_down(self):
+        clue = self.x.clues[-1]
+        assert clue.coord.direction == XWDirection.DOWN
+        fill = self.x.get_fill(clue)
+        self.assertListEqual(fill, [None, None, None])
+
+        self.x.solution[clue.box_indices[1]] = 'A'
+        fill = self.x.get_fill(clue)
+        self.assertListEqual(fill, [None, 'A', None])
+
+    def test_set(self):
+        clue = self.x.clues[0]
+        self.x.set_fill(clue, 'HELLO')
+        fill = self.x.get_fill(clue)
+        self.assertListEqual(fill, ['H', 'E', 'L', 'L', 'O'])
+
+
+if __name__ == "__main__":
+    unittest.main()
